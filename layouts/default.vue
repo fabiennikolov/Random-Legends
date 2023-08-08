ab45ddef837392c0f87e59351e566f8718a79183
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-08 16:31:33 +0300

    chaningn default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,8 @@
 <template>
+  <Nav />
   <main class="px-10 py-20 text-center">
     <slot />
     <Footer />
-    <div class="mx-auto mt-5 text-center text-sm opacity-25">
-      [Default Layout]
-    </div>
+    <div class="mx-auto mt-5 text-center text-sm opacity-25" />
   </main>
 </template>
