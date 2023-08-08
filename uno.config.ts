e18cb1d0002e044fa67330b8c8ec5b9bb569fa49
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-08 16:31:25 +0300

    changing uno css btn

--- uno.config.ts
+++ uno.config.ts
@@ -11,8 +11,7 @@ import {
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['btn', 'px-4 py-1 rounded inline-block bg-purple-7 text-white cursor-pointer hover:bg-black disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
   ],
   presets: [
     presetUno(),
