0bbf46e57e78dd8390dae15a0c15e05f07802e29
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-09 11:46:03 +0300

    adding input entry component

---
+++ components/Form/Input.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const modelValue = defineModel<string>()
+</script>
+
+<template>
+  <input
+    v-model="modelValue"
+    class="my-1 w-full truncate border-b-1 border-gray rounded-t-lg bg-transparent px-5 py-2 text-white outline-none transition-colors-300 focus:(bg-gray/9 ring-none)"
+  >
+</template>
