48cf6cecfcb8d46605ba391f23218979a16df44f
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-10 17:15:33 +0300

    creating tournament card component

---
+++ components/TournamentCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface TourInfo {
+  title: string
+  description: string
+  image: string
+  link: string
+}
+
+defineProps<TourInfo>()
+</script>
+
+<template>
+  <div
+    b="1 transparent" class="max-w-75 w-full overflow-hidden b-1px b-white/20 rounded-3 p-4 shadow-xl drop-shadow-xl space-y-6 hover:bg-gray/7 sm:p-8"
+  >
+    <h4 class="text-(center 2xl) font-(extrabold) leading-relaxed">
+      {{ title }}
+    </h4>
+    <div flex="~ col" class="items-center gap-3">
+      <div class="h-40 w-50">
+        <img :src="image" class="mx-auto h-full object-contain">
+      </div>
+      <a
+        class="mx-auto inline-block b-2 b-black/70 b-transparent rounded-2 bg-blue-1/3 px1 py1 text-sm font-medium shadow-md drop-shadow-md transition-all-300 hover:bg-gray/10"
+        :href="link" target="_blank"
+      >
+        {{ link }}</a>
+    </div>
+    <p v-if="description" class="mt-10px text-center leading-7">
+      {{ description }}
+    </p>
+  </div>
+</template>
