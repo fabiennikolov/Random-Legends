7c23165a6b46b8fc0d11962bffa4c98f3a8c78a0
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-10 12:48:39 +0300

    adding transparency and blur for nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -8,18 +8,16 @@ function toggleMobileMenu() {
 
 <template>
   <div class="nav-container">
-    <div class="mx-auto max-w-screen-2xl flex items-center justify-between px-4">
+    <div class="mx-auto max-w-screen-2xl flex items-center justify-between border-b-1 border-white/9 px-4">
       <div class="flex justify-center gap-8 lg:text-1rem">
         <NuxtLink to="/">
           <img src="@/logo.png" class="my-3 inline-block max-w-45">
         </NuxtLink>
       </div>
       <div class="flex items-center justify-between space-x-4">
-        <!-- Burger Menu Icon -->
         <button class="text-white lg:hidden" @click="toggleMobileMenu">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="#888888" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6.001h18m-18 6h18m-18 6h18" /></svg>
         </button>
-        <!-- Desktop Menu Links -->
         <div class="hidden text-sm text-white/80 lg:flex space-x-4">
           <NuxtLink to="/faq" @click="toggleMobileMenu">
             FAQ
@@ -30,7 +28,6 @@ function toggleMobileMenu() {
         </div>
       </div>
     </div>
-    <!-- Mobile Menu Container -->
     <transition name="fade">
       <div v-if="showMobileMenu" class="p-4 text-white lg:hidden divide-y">
         <NuxtLink to="/faq" class="my-2 block" @click="toggleMobileMenu">
@@ -49,6 +46,8 @@ function toggleMobileMenu() {
   position: sticky;
   top: 0;
   z-index: 1000;
+  background-color: rgba(0, 0, 0, 0);
+  backdrop-filter: blur(2px);
 }
 
 a:hover {
