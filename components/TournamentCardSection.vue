16129313a5c637a527a6d15216e963a8854b140b
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-10 17:15:44 +0300

    creating tournament card section

---
+++ components/TournamentCardSection.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface TourInfo {
+  title: string
+  description: string
+  image: string
+  link: string
+}
+
+defineProps<{
+  cards: TourInfo[]
+}>()
+</script>
+
+<template>
+  <div class="grid mx-auto max-w-300 justify-center gap-6 p-4 lg:grid-cols-4 lt-lg:w-fit">
+    <TournamentCard
+      v-for="card in cards" :key="card.title"
+      v-bind="card"
+    />
+  </div>
+</template>
