455a71e301c11334cda44f66c1e3c6d0c2bf8277
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-11 14:35:20 +0300

    adding rules page

---
+++ pages/Rules.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="mt-10 text-center text-sm lg:text-7 md:text-5">
+    The rules, that everyone must follow during the tournament. You can see the list also in the <a class="leading-7 text-#5865F2 underline" href="https://discord.gg/3x9EVjfUjC" target="_blank">discord</a> channel
+  </h1>
+</template>
