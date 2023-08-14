3dbd25b35c03e6098b1a41a80c99bb8b60c898f9
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-14 15:16:19 +0300

    creating newsletter subscription

--- components/Subscribtion.vue
+++ components/Subscribtion.vue
@@ -2,18 +2,45 @@
 const form = reactive({
   email: '',
 })
+
+const submitted = ref(false)
+
+const isFormFilled = computed(() => {
+  return Object.values(reactiveOmit(form)).every(field => !!field)
+})
+async function submit() {
+  if (!isFormFilled.value)
+    return
+
+  submitted.value = true
+  form.reset()
+}
 </script>
 
 <template>
-  <div grid mxa mt15 justify-center>
-    <h1 text-7>
-      Sign up for our newsletter
-    </h1>
-    <FormInput id="Email" v-model="form.email" type="email" placeholder="Email" />
-    <button
-      class="relative mt10 w-full b-1 b-gray rounded-full bg-black/2 px-5 py-4 text-center font-500 text-white transition-colors-250 after:absolute after:z-0 hover:bg-gray/9 after:transition-all-250 hover:after:opacity-65" type="submit"
-    >
-      Send
-    </button>
-  </div>
+  <form class="mxa my-10 max-w-275 rounded-3" @submit.prevent="submit">
+    <div class="grid mx-auto max-w-330 overflow-hidden b-1 b-white/7 rounded-2 bg-gray/7 text-white md:grid-cols-2">
+      <img src="/esport3.jpg" alt="Newsletter image" class="w-full md:h-full md:object-cover">
+      <div class="px-6 py-10 space-y-8">
+        <div>
+          <h3 class="text-center text-6 font-bold leading-7 md:text-7">
+            Subscribe and recieve notifications for Upcomming tournaments
+          </h3>
+          <p class="mt-2 text-center text-white/80 lt-md:text-3.5">
+            We wont spam you, pinky promise
+          </p>
+        </div>
+        <div space-y-3="">
+          <FormInput id="Email" v-model="form.email" type="email" placeholder="Email" mb-4 />
+
+          <button class="w-full flex items-center justify-center gap-2 border-1 rounded-full px-2 py-3 text-4.5 font-500 text-white/80 hover:bg-white/7">
+            <span uppercase lt-md:text-3.5>Subscribe</span>
+          </button>
+          <p v-if="submitted" class="mt5 text-green-5">
+            Thank you for subscribing !
+          </p>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
