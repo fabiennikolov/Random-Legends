b6d9907bd220764649cde05577efd00d65aad342
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-15 14:08:47 +0300

    adding UniIcon component

---
+++ components/UnoIcon.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { UnoCssString } from '~/types'
+
+defineProps<{
+  icon: UnoCssString | string
+}>()
+</script>
+
+<template>
+  <i class="inline-block" :class="icon" />
+</template>
