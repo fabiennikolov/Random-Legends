c5ae9f1b8835ef877f0829367da617bf504714ae
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-15 17:09:40 +0300

    using highlight item in newsletter and restyling

--- components/Newsletter.vue
+++ components/Newsletter.vue
@@ -4,9 +4,9 @@
 
 <template>
   <div class="relative isolate overflow-hidden py-16 text-sm lg:py-32 sm:py-24 lg:text-4">
-    <div class="mx-auto max-w-6xl px-6 lg:px-6">
-      <div class="grid grid-cols-1 mx-auto max-w-2xl gap-x-8 gap-y-16 lg:grid-cols-2 lg:max-w-none">
-        <div class="max-w-xl lg:max-w-lg">
+    <div class="mx-auto max-w-6xl px-6">
+      <div class="grid grid-cols-1 mx-auto max-w-2xl gap-16 lg:grid-cols-2 lg:max-w-none">
+        <div>
           <h2 class="text-center text-3xl font-bold tracking-tight text-white lg:text-start sm:text-4xl">
             Subscribe to our newsletter.
           </h2>
@@ -15,34 +15,45 @@
           </p>
           <div class="mt-6 flex gap-x-4">
             <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto border-0 rounded-md bg-white/9 px-3.5 py-2 text-white shadow-sm ring-1 ring-white/10 ring-inset sm:text-sm sm:leading-6" placeholder="Enter your email">
-            <button type="submit" class="btn">
+            <CoolButton>
               Subscribe
-            </button>
+            </CoolButton>
           </div>
         </div>
-        <div class="flex gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
-          <div class="flex flex-col items-start">
-            <div class="rounded-md bg-white/5 p-1 ring-1 ring-white/10">
-              <UnoIcon icon="i-material-symbols-calendar-month-outline" h-8 w-8 />
-            </div>
-            <h1 class="mt-4 font-semibold text-white">
+
+        <div class="grid grid-cols-[repeat(auto-fill,_minmax(12rem,1fr))] gap-10">
+          <HighlightItem icon="i-material-symbols-calendar-month-outline">
+            <template #title>
               Weekly articles
-            </h1>
-            <p class="mt-2 leading-7 text-gray-400">
+            </template>
+            <template #description>
               You will receive early access to our tournaments and events.
-            </p>
-          </div>
-          <div class="flex flex-col items-start">
-            <div class="rounded-md bg-white/5 p-1 ring-1 ring-white/10">
-              <UnoIcon icon="i-heroicons:hand-raised" h-8 w-8 />
-            </div>
-            <h1 class="mt-4 font-semibold text-white">
+            </template>
+          </HighlightItem>
+          <HighlightItem icon="i-material-symbols-calendar-month-outline">
+            <template #title>
+              Weekly articles
+            </template>
+            <template #description>
+              You will receive early access to our tournaments and events.
+            </template>
+          </HighlightItem>
+          <HighlightItem icon="i-material-symbols-calendar-month-outline">
+            <template #title>
+              Weekly articles
+            </template>
+            <template #description>
+              You will receive early access to our tournaments and events.
+            </template>
+          </HighlightItem>
+          <HighlightItem icon="i-heroicons:hand-raised">
+            <template #title>
               No spam
-            </h1>
-            <p class="mt-2 leading-7 text-gray-400">
-              We wont send you useless stuff.
-            </p>
-          </div>
+            </template>
+            <template #description>
+              We won't send you useless stuff.
+            </template>
+          </HighlightItem>
         </div>
       </div>
     </div>
