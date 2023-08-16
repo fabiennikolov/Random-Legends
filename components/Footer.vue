d837d454a5c150a075a469f787642fc4c59aeb89
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-16 15:11:56 +0300

    fixing footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -16,13 +16,13 @@ const socialLinks: SocialLink[] = [
 
 <template>
   <footer class="border-t-1 b-white/9 py-2 text-white/80">
-    <div class="mx-auto max-w-1120px flex flex-1 flex-col items-center justify-between px-4 py-2 text-center text-xs text-gray-400 md:flex-row space-y-2 md:text-left md:space-y-0">
+    <div class="mx-auto max-w-6xl flex flex-1 flex-col items-center justify-between px-4 py-2 text-center text-xs text-gray-400 md:flex-row space-y-2 md:text-left md:space-y-0">
       <div>
         Copyright © 2023 Random Legends. All rights reserved. Random Legends isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
       </div>
       <div flex flex-col items-end>
         <ul>
-          <li mb5 flex gap-2>
+          <li flex gap-2>
             <a href="">
               <UnoIcon class="w8 h8" icon="i-ic-round-facebook" />
             </a>
@@ -44,7 +44,7 @@ const socialLinks: SocialLink[] = [
     </div>
     <div class="mx-auto max-w-1120px flex items-center justify-center px-4 py-2 text-center text-gray-400 md:flex-row lg:text-5">
       <section>
-        <ul flex gap-5 lg:py-5>
+        <ul flex gap-5 lg:py-5 uppercase>
           <li>
             <NuxtLink to="/">
               Home
