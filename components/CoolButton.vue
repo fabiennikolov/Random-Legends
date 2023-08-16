fc07833b84b19741cd2c4c84f7de97f091447839
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-16 15:11:47 +0300

    importing cva to cool button

--- components/CoolButton.vue
+++ components/CoolButton.vue
@@ -1,11 +1,37 @@
+<script setup lang="ts">
+import { cva } from 'class-variance-authority'
+
+withDefaults(defineProps<{
+  intent?: 'primary' | 'accent' | 'unstyled'
+  overlayClass?: string
+}>(), {
+  intent: 'primary',
+})
+
+const buttonClass = computed(() => {
+  return cva(
+    'text-white border-white',
+    {
+      variants: {
+        intent: {
+          primary: '',
+          accent: '',
+        },
+      },
+    })
+})
+</script>
+
 <template>
   <button
     type="submit" class="group relative isolate overflow-hidden btn"
+    :class="{ ' text-white border-1 border-white': intent !== 'unstyled' }"
   >
     <div
-      class="absolute -inset-4 -z-1"
-      bg-white text="#222222" rounded="50%"
-      transition-all-500 translate-y="80%" group-hover="translate-y-0"
+      class="absolute -top-4 -bottom-4 -left-10 -right-10 -z-1"
+      :class="[{ 'bg-white text-[#222222]': !overlayClass }, overlayClass]"
+      rounded="50%"
+      transition-all-500 translate-y="100%" group-hover="translate-y-0"
       flex items-center justify-center
     >
       <slot />
