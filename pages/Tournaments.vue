a758f6933d3bfd2a6164ecf51ae3d97f4454bddf
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-16 15:13:02 +0300

    adding cool button to tournaments page

--- pages/Tournaments.vue
+++ pages/Tournaments.vue
@@ -29,7 +29,11 @@ const cards = ref<TourInfo[]>([
   </h2>
   <p my1 mb-15 text-center text-6>
     Every tournament is streamed on
-    <a href="" tag="a" class="ml1 block inline-block justify-center b-1px border-white rounded-2 bg-violet-800 px2 text-center transition-all-300 hover:bg-violet-900">twitch<svg class="float-right mt1 pl1" xmlns="http://www.w3.org/2000/svg" width="25" height="27" viewBox="0 0 24 24"><path fill="white" d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z" /></svg></a>
+    <a href="https://www.twitch.tv/" target="_blank">
+      <CoolButton class="ml2" bg="#6441A4" border-violet-6 text-white hover="border-white"> Twitch
+        <UnoIcon icon="i-mdi-twitch" ml1 />
+      </CoolButton>
+    </a>
   </p>
   <TournamentCardSection :cards="cards" />
   <Newsletter />
