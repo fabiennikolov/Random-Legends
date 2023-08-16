51f43210b9c958fbf734962a867906699a6b093d
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-16 15:43:04 +0300

    using the cool button in partner component

--- components/PartnerBenefits.vue
+++ components/PartnerBenefits.vue
@@ -9,15 +9,15 @@
 
           <div class="mt-6 flex gap-x-4">
             <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto border-0 rounded-md bg-white/9 px-3.5 py-2 text-white shadow-sm ring-1 ring-white/10 ring-inset sm:text-sm sm:leading-6" placeholder="Enter your email">
-            <button type="submit" class="btn">
+            <CoolButton type="submit" class="btn">
               Become Partner
-            </button>
+            </CoolButton>
           </div>
         </div>
         <div class="flex gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
           <div class="flex flex-col items-start">
             <div class="rounded-md bg-white/5 p-1 ring-1 ring-white/10">
-              <UnoIcon icon="i-solar-hand-money-outline" />
+              <UnoIcon icon="i-solar-hand-money-outline" w8 h8 />
             </div>
             <h1 class="mt-4 font-semibold text-white">
               Earn a comission
@@ -28,7 +28,7 @@
           </div>
           <div class="flex flex-col items-start">
             <div class="rounded-md bg-white/5 p-1 ring-1 ring-white/10">
-              <UnoIcon icon="i-carbon-collaborate" />
+              <UnoIcon icon="i-carbon-collaborate" w8 h8 />
             </div>
             <h1 class="mt-4 font-semibold text-white">
               Collaboration
@@ -39,7 +39,7 @@
           </div>
           <div class="flex flex-col items-start">
             <div class="rounded-md bg-white/5 p-1 ring-1 ring-white/10">
-              <UnoIcon icon="i-iconoir-community" />
+              <UnoIcon icon="i-iconoir-community" w8 h8 />
             </div>
             <h1 class="mt-4 font-semibold text-white">
               Access
