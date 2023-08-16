685326e5939121f663c0947b045380f5b3ab0f1d
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-08-16 15:11:19 +0300

    fixing btn

--- uno.config.ts
+++ uno.config.ts
@@ -11,7 +11,7 @@ import {
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded-2 inline-block text-white  cursor-pointer b-1 b-white transition-all-300 hover:bg-gray/10 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['btn', 'px-4 py-1 rounded-2 inline-block  cursor-pointer b-1 transition-all-300 disabled:cursor-default disabled:opacity-50'],
   ],
   presets: [
     presetUno(),
